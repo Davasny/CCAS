d69a5dc2364b40e2f8d4e0bfc5c980fdc0c3c8d1
Davasny <davasny@gmail.com>
2017-05-27 22:38:38 +0200

    Added support for ETH

--- ccas/models/currency/eth.py
+++ ccas/models/currency/eth.py
@@ -1,18 +1,32 @@
 import urllib
 import urllib.request
 import json
+from decimal import *
+from ccas.models.exchanges import poloniex
+
 
 def get_balance(list_of_address):
     string_of_addresses = ''
     for address in list_of_address:
-        string_of_addresses = string_of_addresses + '|' + address
+        string_of_addresses = address + ',' + string_of_addresses
 
     raw_json = urllib.request.urlopen(
-        'https://blockchain.info/pl/balance?active=' + string_of_addresses)
+        'https://api.etherscan.io/api?module=account&action=balancemulti&address=' + string_of_addresses[:-1])
     parsed = json.loads(raw_json.read().decode('utf=8'))
 
-    final_balances = {}
-    for address in parsed:
-        final_balances[address] = parsed['final_balance'] / (10**8)
+    all_balances = [([0] * 5) for i in range(len(parsed))]
 
-    return final_balances
+    price = get_price()
+
+    i = 0
+    for account in parsed:
+        all_balances[i][0] = "ETH"
+        all_balances[i][1] = parsed['result'][i-1]['account']
+        all_balances[i][2] = Decimal(parsed['result'][i-1]['balance']) / (10**18)
+        all_balances[i][3] = price
+        all_balances[i][4] = "CURRENCY"
+        i += 1
+    return all_balances
+
+def get_price():
+    return poloniex.get_price("ETH")
