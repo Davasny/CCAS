d2f70896ff373c4dc1c17eac3c37095f5057a071
Davasny <davasny@gmail.com>
2017-05-28 17:13:40 +0200

    Added loop

--- ccas/controller/controller.py
+++ ccas/controller/controller.py
@@ -1,60 +1,56 @@
 import collections
 from ccas import app
 from ccas.models import currency, exchanges
-from flask import render_template, request
+from flask import render_template, request, make_response, redirect
 from ccas.models.currency import wallets, groups
 from decimal import *
-import json
+import configparser
+import hashlib
+
 
 @app.route('/')
 @app.route('/dashboard')
 def dashboard():
-    # wybierz grupę
-    # pobierz wszystkie salda grupy
-    # sumuj wszystkie salda do jednego
-    # dodaj do balances
+    config = configparser.ConfigParser()
+    config.read("ccas/config.ini")
+    supported_currency = config["Currency"]["supportedCurrency"].split(",")
+    supported_exchanges = config["Exchanges"]["supportedExchanges"].split(",")
+    balances = []
 
 
-    # loooop through all currency
-    new_currency = "BTC"
-    all_wallets = wallets.get_raw_addresses(new_currency) # wallets without group
-    balances = currency.get_details(new_currency, all_wallets, "balance")
-
-    # get group
-    all_groups = groups.get_all_groups(new_currency)
-
-
-    # send wallets to get balances
-    group_balances = []
-    for group in all_groups:
-        all_wallets = wallets.get_address_by_group(group[0])  # wallets without group
-
-        if all_wallets:
-            group_balances.append(new_currency)
-            group_balances.append(group[1])
-            group_balances.append(0)
-
-            group_details = currency.get_details(new_currency, all_wallets, "balance")
-            if group_details:
-                for wallet in group_details:
-                    group_balances[2] += Decimal(wallet[2])
-                group_balances.append(group_details[0][3])
-                group_balances.append("GROUP")
     tmp_group = []
-    tmp_group.append(group_balances)
+    for new_currency in supported_currency:
+        all_wallets = wallets.get_raw_addresses(new_currency) # wallets without group
+        balances.extend(currency.get_details(new_currency, all_wallets, "balance"))
 
+        # get group
+        all_groups = groups.get_all_groups(new_currency)
+
+        # send wallets to get balances
+        group_balances = []
+        for group in all_groups:
+            all_wallets = wallets.get_address_by_group(group[0])  # wallets without group
+
+            if all_wallets:
+                group_balances.append(new_currency)
+                group_balances.append(group[1])
+                group_balances.append(0)
+
+                group_details = currency.get_details(new_currency, all_wallets, "balance")
+                if group_details:
+                    for wallet in group_details:
+                        group_balances[2] += Decimal(wallet[2])
+                    group_balances.append(group_details[0][3])
+                    group_balances.append("GROUP")
+                tmp_group.append(group_balances)
+    balances.extend(tmp_group)
 
     # loooop through all exchanges
-    exchane = exchanges.get_balances("poloniex")
+    for new_exchange in supported_exchanges:
+        balances.extend(exchanges.get_balances(new_exchange))
 
-
-    # join CURRENCY, GROUPS and EXCHANGES
-    balances.extend(tmp_group)
-    balances.extend(exchane)
-    #print(json.dumps(balances))
 
     # [CURRENCY, PLACE, AMOUNT, PRICE]
-
     return render_template('dashboard.html', balances=balances)
 
 @app.route('/exchanges')
