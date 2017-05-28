aa732178c5e9ad0f488971a085a3df00649de656
Davasny <davasny@gmail.com>
2017-05-28 17:12:20 +0200

    Changed secret_key

--- ccas/__init__.py
+++ ccas/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+import hashlib, time
+
+
 app = Flask('ccas')
-#app.config['SECRET_KEY'] = 'random'
+app.config['SECRET_KEY'] = str(hashlib.sha256(str(time.time()).encode('utf-8')).hexdigest())
 
 import ccas.models
 import ccas.controller
