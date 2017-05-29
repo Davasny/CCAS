9ea4fb6603ce935176b7b000788b0c8110c8aab8
Davasny <davasny@gmail.com>
2017-05-29 22:47:19 +0200

    Added get_addresses_with_names

--- ccas/models/currency/wallets.py
+++ ccas/models/currency/wallets.py
@@ -7,6 +7,17 @@ def get_raw_addresses(currency):
         response.append(row[0])
     return response
 
+def get_addresses_with_names(currency):
+    raw_data = database.new_query("select wallets.address, wallets.name from wallets where (parent_group is NULL or parent_group = '') AND currency='"+ currency +"' ;")
+    response = []
+    for row in raw_data:
+        tmp_list = []
+        tmp_list.append(row[0])
+        tmp_list.append(row[1])
+
+        response.append(tmp_list)
+    return response
+
 def get_addresses(currency):
     raw_data = database.new_query("select wallets.address from wallets left join groups on groups.id=wallets.parent_group WHERE wallets.currency='"+ currency +"' order by wallets.currency;")
     response = []
