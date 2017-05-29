fc8bbc143ae80aab42147c5705a858592275516d
Davasny <davasny@gmail.com>
2017-05-29 18:41:23 +0200

    Added get_exchanges(), changed get_balances()

--- ccas/models/exchanges/__init__.py
+++ ccas/models/exchanges/__init__.py
@@ -1,6 +1,10 @@
 from . import poloniex
+from ccas.models import database
 
-
-def get_balances(exchange):
+def get_balances(exchange, public_key, secret_key):
     if exchange == "poloniex":
-        return poloniex.get_balances()
+        return poloniex.get_balances(public_key, secret_key)
+
+def get_exchanges():
+    response = database.new_query("SELECT id, exchange FROM exchanges_api_keys;")
+    return list(response)
