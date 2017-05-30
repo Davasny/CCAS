1e15a63bea34b297b4319659c51e15c87de792b3
Davasny <davasny@gmail.com>
2017-05-30 09:11:12 +0200

    Changed insert_new to new_argument_query

--- ccas/models/database.py
+++ ccas/models/database.py
@@ -8,7 +8,7 @@ def new_query(command):
     table = cur.fetchall()
     return table
 
-def insert_new(command, args=()):
+def new_argument_query(command, args=()):
     operating_database = sqlite3.connect("ccas/ccas_main_database.db")
     cur = operating_database.cursor()
     cur.execute(command, args)
