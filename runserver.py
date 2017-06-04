2525342443646ab07adf979fb459174d4f2b34f9
Davasny <davasny@gmail.com>
2017-06-04 12:33:32 +0200

    Much better

--- runserver.py
+++ runserver.py
@@ -1,4 +1,31 @@
 from ccas import app
+import optparse
+
+
+def flaskrun(default_host="localhost",
+                  default_port="5000"):
+
+    parser = optparse.OptionParser()
+    parser.add_option("-H", "--host",
+                      help="Hostname/IP of CCAS " + \
+                           "[default %s]" % default_host,
+                      default=default_host)
+    parser.add_option("-P", "--port",
+                      help="Port for the CCAS " + \
+                           "[default %s]" % default_port,
+                      default=default_port)
+    parser.add_option("-d", "--debug",
+                      action="store_true", dest="debug",
+                      help=optparse.SUPPRESS_HELP)
+
+    options, _ = parser.parse_args()
+
+    app.run(
+        debug=options.debug,
+        host=options.host,
+        port=int(options.port)
+    )
+
 
 if __name__ == '__main__':
-    app.run('0.0.0.0',debug=True, threaded=True)
+    flaskrun()
